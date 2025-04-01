e0e948432e34f1d51cca68fc1820acf420d49f00
Pedro Hennrique <pedrosilveira07@outlook.com>
2025-04-01 10:39:03 -0300

    refactor: change primary color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,25 +3,24 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4'
-const tintColorDark = '#fff'
+const primaryColor = '#FB5A2A'
 
 export const Colors = {
   light: {
     text: '#11181C',
     background: '#fff',
-    tint: tintColorLight,
+    tint: primaryColor,
     icon: '#687076',
     tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight
+    tabIconSelected: primaryColor
   },
   dark: {
     text: '#ECEDEE',
     background: '#151718',
-    tint: tintColorDark,
+    tint: primaryColor,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark
+    tabIconSelected: primaryColor
   }
 }
 
